a5eec7e1bf1b007c44687b09d51f88620cd28655
Justus Kandzi <justus.kandzi@gmail.com>
2017-02-09 12:13:23 +0100

    add carthage support

---
+++ VersionsTracker/VersionsTracker.h
@@ -0,0 +1,19 @@
+//
+//  VersionsTracker.h
+//  VersionsTracker
+//
+//  Created by Justus Kandzi on 09/02/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VersionsTracker.
+FOUNDATION_EXPORT double VersionsTrackerVersionNumber;
+
+//! Project version string for VersionsTracker.
+FOUNDATION_EXPORT const unsigned char VersionsTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VersionsTracker/PublicHeader.h>
+
+
